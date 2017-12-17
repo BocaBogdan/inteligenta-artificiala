bc75022e97ce3b99dad65baa8c00ccd3572298ce
simon.stefi <simon.stefi@yahoo.com>
2017-12-17 14:27:23 +0200

    doc pt grupa a1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {IaHeaderComponent} from "./ia-header/ia-header.component";
+import {IaHeaderComponent} from './ia-header/ia-header.component';
 import {IaContentComponent} from './ia-content/ia-content.component';
-import {B1PageComponent} from './pages/b1-page/b1-page.component';
-import {AppRoutingModule} from "./app-routing-module/app-routing-module.module";
-import {HomeComponent} from "./pages/home/home.component";
-import {B2PageComponent} from './pages/b2-page/b2-page.component';
-import {B3PageComponent} from './pages/b3-page/b3-page.component';
-import {B4PageComponent} from './pages/b4-page/b4-page.component';
-import {B5PageComponent} from './pages/b5-page/b5-page.component';
-import {B6PageComponent} from './pages/b6-page/b6-page.component';
-import {B7PageComponent} from './pages/b7-page/b7-page.component';
-
+import {B1PageComponent} from './pages/B/b1-page/b1-page.component';
+import {AppRoutingModule} from './app-routing-module/app-routing-module.module';
+import {HomeComponent} from './pages/home/home.component';
+import {A1PageComponent} from './pages/A/a1-page/a1-page.component';
+import {B2PageComponent} from './pages/B/b2-page/b2-page.component';
+import {B3PageComponent} from './pages/B/b3-page/b3-page.component';
+import {B4PageComponent} from './pages/B/b4-page/b4-page.component';
+import {B5PageComponent} from './pages/B/b5-page/b5-page.component';
+import {B6PageComponent} from './pages/B/b6-page/b6-page.component';
+import {B7PageComponent} from './pages/B/b7-page/b7-page.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +22,7 @@ import {B7PageComponent} from './pages/b7-page/b7-page.component';
     IaContentComponent,
     B1PageComponent,
     HomeComponent,
+    A1PageComponent,
     B2PageComponent,
     B3PageComponent,
     B4PageComponent,
---
+++ src/app/pages/A/a1-page/a1-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ia-a1-page',
+  templateUrl: './a1-page.component.html',
+  styleUrls: ['./a1-page.component.css']
+})
+export class A1PageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
