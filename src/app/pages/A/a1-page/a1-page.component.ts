79059f91bcae8685fa460d61ec4abbd6bb664d2c
simon.stefi <simon.stefi@yahoo.com>
2018-01-14 20:02:40 +0200

    styling changes

--- src/app/pages/A/a1-page/a1-page.component.ts
+++ src/app/pages/A/a1-page/a1-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ia-a1-page',
-  templateUrl: './a1-page.component.html',
-  styleUrls: ['./a1-page.component.css']
+  templateUrl: './a1-page.component.html'
 })
 export class A1PageComponent implements OnInit {
 
